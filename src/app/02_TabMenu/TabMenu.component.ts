3937d982f9752dc49152134aab315fe04bc54545
Rafael-Go7 <aixp@outlook.de>
2023-06-02 18:58:12 +0200

    02_TabMenu.component - Asia + Pasta + Indian arrays added almost finished

--- src/app/02_TabMenu/TabMenu.component.ts
+++ src/app/02_TabMenu/TabMenu.component.ts
@@ -75,7 +75,7 @@ ngOnInit(): void {
 Pizzas : FoodJSON [] = { meals : [{
                                   "Salami": [{
                                       img: "./assets/img/meals/1_pizzas/pizza_salami_1280.jpg",
-                                      restaurant: "Majster Pizza",
+                                      restaurant: "Magic Pizza",
                                       dish: "Pizza Salami",
                                       ingredients: "with Mozarella, Hot Pepper, Tomatoe Sauce, Pepper, Sourdough, from the woodstove...",
                                       price: 8.5,
@@ -83,7 +83,7 @@ Pizzas : FoodJSON [] = { meals : [{
                                   }],
                                   "Milano": [{
                                       img: "./assets/img/meals/1_pizzas/pizza_Milano2_1280 x 853.jpg",
-                                      restaurant: "Majster Pizza",
+                                      restaurant: "Magic Pizza",
                                       dish: "Pizza Milano",
                                       ingredients: "with Mozarella, Tomatoe Sauce, Mushrooms, Ham, Salami, Sourdough, from the woodstove...",
                                       price: 9,
@@ -91,7 +91,7 @@ Pizzas : FoodJSON [] = { meals : [{
                                   }],
                                   "Veggie": [{
                                     img: "./assets/img/meals/1_pizzas/veggie_1_723 x 642_modified.jpg",
-                                    restaurant: "Majster Pizza",
+                                    restaurant: "Magic Pizza",
                                     dish: "Pizza Veggie",
                                     ingredients: "with Mozarella, Tomatoe Sauce, Zucchini, Olives, Egg Plant, Rucola, from the woodstove...",
                                     price: 9,
@@ -99,7 +99,7 @@ Pizzas : FoodJSON [] = { meals : [{
                                    }],
                                   "Four Seasons": [{
                                     img: "./assets/img/meals/1_pizzas/pizza_four seasons_1280x853.jpg",
-                                    restaurant: "Majster Pizza",
+                                    restaurant: "Magic Pizza",
                                     dish: "Pizza Four Seasons",
                                     ingredients: "with Mozarella, Tomatoe Sauce, Onions, Paprica, Olives, Salami, Mushrooms, Sourdough, from the woodstove...",
                                     price: 9,
@@ -107,7 +107,7 @@ Pizzas : FoodJSON [] = { meals : [{
                                     }],
                                   "Diavolo": [{
                                     img: "./assets/img/meals/1_pizzas/diavolo_1_1220 x 598_modified.jpg",
-                                    restaurant: "Majster Pizza",
+                                    restaurant: "Magic Pizza",
                                     dish: "Pizza Diavolo",
                                     ingredients: "with Mozarella, Tomatoe Sauce, Hot Salami, Pepperoni, Sourdough, from the woodstove...",
                                     price: 9,
@@ -115,7 +115,7 @@ Pizzas : FoodJSON [] = { meals : [{
                                     }],
                                   "Mozarella": [{
                                     img: "./assets/img/meals/1_pizzas/mozarella_1_1192 x 1084_modified.jpg",
-                                    restaurant: "Majster Pizza",
+                                    restaurant: "Magic Pizza",
                                     dish: "Pizza Mozarella",
                                     ingredients: "with Mozarella, Tomatoe Sauce, Tomatoes, Basil, Sourdough, from the woodstove...",
                                     price: 9,
@@ -178,6 +178,163 @@ Salads : FoodJSON [] = { meals : [{
                             }
 
 
+Asia : FoodJSON [] = { meals : [{
+                              "ThaiShrimps": [{
+                                  img: "./assets/img/meals/3_asia/thaiShrimps_1_1280 x 1162.jpg",
+                                  restaurant: "Chinese Garden",
+                                  dish: "Thai Shrimps Noodles",
+                                  ingredients: "Thai Noodles, Shrimps, Eggs, Coriander, Paprica, Chives, Thai Spices",
+                                  price: 15.0,
+                                  amount: 1
+                              }],
+                              "Sushi": [{
+                                  img: "./assets/img/meals/3_asia/sushi3_1_1280 x 853.jpg",
+                                  restaurant: "Chinese Garden",
+                                  dish: "Sushi Neo-Tokyo",
+                                  ingredients: "Rice, Algae, Avocado, Salmon, Different Fish, Iceberg Lettuce, Parsley, Herbs",
+                                  price: 14,
+                                  amount: 1
+                              }],
+                              "Taiwan": [{
+                                img: "./assets/img/meals/3_asia/noodleSoup_1_1280 x 743_modified.jpg",
+                                restaurant: "Chinese Garden",
+                                dish: "Taiwanese Noodle Soup (Hot)",
+                                ingredients: "Taiwanese Noodles, Beef, Coriander, Green Onion, Vegetables, Hot Spice",
+                                price: 9,
+                                amount: 1
+                               }],
+                              "Viet": [{
+                                img: "./assets/img/meals/3_asia/vietNoodleBowl_1_892 x 887_modified.jpg",
+                                restaurant: "Chinese Garden",
+                                dish: "Viet Bowl",
+                                ingredients: "Vietnamese Noodles, Chicken, Vgetables, Onion, Parsley, Peppermint, ",
+                                price: 14,
+                                amount: 1
+                                }],
+                              "ChinGong": [{
+                                img: "./assets/img/meals/3_asia/ChinGongBeef_1_1280 x 960.jpg",
+                                restaurant: "Chinese Garden",
+                                dish: "Chin Gong Beef",
+                                ingredients: "Rice, Beef, Chinese Sweet-Sour Sauce, Peanuts, Sesame, Parsley, Iceberg Lettuce, Vegetables",
+                                price: 12,
+                                amount: 1
+                                }],
+                              "Mala": [{
+                                img: "./assets/img/meals/3_asia/malaChicken_1_773 x 588_modified.jpg",
+                                restaurant: "Chinese Garden",
+                                dish: "Mala Chicken",
+                                ingredients: "Chicken, Tofu Cubes (fried), Green Beans, Plums, Spices",
+                                price: 11,
+                                amount: 1
+                            }],
+                            }]
+}
+
+
+Pasta : FoodJSON [] = { meals : [{
+                              "Basil": [{
+                                  img: "./assets/img/meals/4_pasta/basilTagliatelle_1_1280 x 1097.jpg",
+                                  restaurant: "Angelo's",
+                                  dish: "Tagliatelle Basil di Tuscany",
+                                  ingredients: "Tagliatelle (homemade Style), Basil Pesto, Parmesan Cheese, Fresh Basil",
+                                  price: 13.0,
+                                  amount: 1
+                              }],
+                              "FruttiMare": [{
+                                  img: "./assets/img/meals/4_pasta/fruttiMare_1280xx 853.jpg",
+                                  restaurant: "Angelo's",
+                                  dish: "Pasta Frutti di Mare",
+                                  ingredients: "Spaghetti di Mare, Frutti di Mare, Fresh Venus Shells, Onions, Parsley",
+                                  price: 19,
+                                  amount: 1
+                              }],
+                              "CremaPasta": [{
+                                img: "./assets/img/meals/4_pasta/creamPasta_1_853 x 822_modified.jpg",
+                                restaurant: "Angelo's",
+                                dish: "Crema Pasta",
+                                ingredients: "Spaghetti, North Italian melting Cheese (aromatic), Cream, Parsley",
+                                price: 9,
+                                amount: 1
+                              }],
+                              "Bolognaise": [{
+                                img: "./assets/img/meals/3_asia/vietNoodleBowl_1_892 x 887_modified.jpg",
+                                restaurant: "Angelo's",
+                                dish: "Spaghetti Bolognaise",
+                                ingredients: "Spaghetti, Meat, Giorgio's Tomatoe Sauce, Parmesan Cheese",
+                                price: 14,
+                                amount: 1
+                                }],
+                              "Salmone": [{
+                                img: "./assets/img/meals/4_pasta/salmone_1_1280 x 853.jpg",
+                                restaurant: "Angelo's",
+                                dish: "Pasta Salmone",
+                                ingredients: "Tagliatelle, Salmone, Rosemary, Basil-Paprica-Cream, Asparagus, 1 Shrimp, Tomatoes, Radishes, Lemon",
+                                price: 20,
+                                amount: 1
+                                }],
+                              "Shrimps": [{
+                                img: "./assets/img/meals/3_asia/malaChicken_1_773 x 588_modified.jpg",
+                                restaurant: "Angelo's",
+                                dish: "Capri Shrimps",
+                                ingredients: "Spaghetti, Shrimps di Capri, Tomatoes, Parmesan Cheese, Fresh Basil",
+                                price: 11,
+                                amount: 1
+                            }],
+                            }]
+}
+
+
+Indian : FoodJSON [] = { meals : [{
+                              "CurryCheese": [{
+                                  img: "./assets/img/meals/5_indian/curryCheese_1_1280 x 853.jpg",
+                                  restaurant: "Taj Mahal",
+                                  dish: "Curry Cream Cheese",
+                                  ingredients: "Rice, Cream Cheese, Curry, Paprica, Ginger, Curcuma, Parsley",
+                                  price: 11.0,
+                                  amount: 1
+                              }],
+                              "FamilyMenu": [{
+                                  img: "./assets/img/meals/5_indian/fullMenuSpices_1_1090 x 835_modified.jpg",
+                                  restaurant: "Taj Mahal",
+                                  dish: "Family Menu Curry",
+                                  ingredients: "Rice, Optional: Chicken or Vegetable Curry, a Pallette of Indian Spices (Curcuma, Cinnamon, Caraway Seeds, Cashew Nuts, Clove, Nutmeg, Anisey, dried Orange Peel)",
+                                  price: 38,
+                                  amount: 1
+                              }],
+                              "Samosa": [{
+                                img: "./assets/img/meals/5_indian/modernSamosa_1_1280 x 550_modified.jpg",
+                                restaurant: "Taj Mahal",
+                                dish: "Modern Samosa",
+                                ingredients: "4x Dumplings with Vegetables (Fried), Paprica Powder, Onions, Parsley, Sweet-Sour Sauce",
+                                price: 12,
+                                amount: 1
+                              }],
+                              "Yoghurt": [{
+                                img: "./assets/img/meals/5_indian/fish_1_1280 x 853.jpg",
+                                restaurant: "Taj Mahal",
+                                dish: "Yoghurt Curry",
+                                ingredients: "Rice, Yoghurt (Spiced), Brocolli, Carrots, Onions, Parsley",
+                                price: 10,
+                                amount: 1
+                                }],
+                              "Pumpkin": [{
+                                img: "./assets/img/meals/5_indian/rice_bowl.jpg",
+                                restaurant: "Taj Mahal",
+                                dish: "Pumpkin Curry",
+                                ingredients: "Rice, Pumpkin, Peanuts, Onions, Carrots, Zucchini",
+                                price: 13,
+                                amount: 1
+                                }],
+                              "Beans": [{
+                                img: "./assets/img/meals/5_indian/beansPan_1_720 x 592_modified.jpg",
+                                restaurant: "Taj Mahal",
+                                dish: "Beans Pan Curry",
+                                ingredients: "Rice, Beans, Cinnamon, Onions, Parsley",
+                                price: 14,
+                                amount: 1
+                            }],
+                            }]
+}
 
 
 }
